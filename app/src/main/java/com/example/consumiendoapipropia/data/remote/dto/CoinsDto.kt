3718e87164ae48e03e4691f33b5d23d04f738764
PrandiFabiel <fabiel098@gmail.com>
2022-07-01 12:21:57 -0400

    Cambiando tipo de dato de dto.

--- app/src/main/java/com/example/consumiendoapipropia/data/remote/dto/CoinsDto.kt
+++ app/src/main/java/com/example/consumiendoapipropia/data/remote/dto/CoinsDto.kt
@@ -3,6 +3,6 @@ package com.example.consumiendoapipropia.data.remote.dto
 data class CoinsDto(
     val monedaId: String = "",
     val descripcion: String = "",
-    val valor: String = " ",
+    val valor: Double = 0.0,
     val imgUrl: String? = ""
 )
