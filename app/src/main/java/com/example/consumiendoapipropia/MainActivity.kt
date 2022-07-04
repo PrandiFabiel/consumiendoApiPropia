97ebf72a8a2fc1040fcebdc980e25be9428d6387
PrandiFabiel <fabiel098@gmail.com>
2022-07-03 22:48:50 -0400

    Ajustando la navegacion.

--- app/src/main/java/com/example/consumiendoapipropia/MainActivity.kt
+++ app/src/main/java/com/example/consumiendoapipropia/MainActivity.kt
@@ -55,7 +55,7 @@ fun CoinApp(
                     )
                 }
                 composable(route = Screen.nameRegistro.route ){
-                    CoinsRegisterScreen(onSave = {navHostController.navigateUp()})
+                    CoinsRegisterScreen(onSave = {navHostController.navigate(Screen.nameList.route)})
                 }
 
             }
